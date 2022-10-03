e265615fe9ceec93982788dac3b0701469d9c20d
Nagaraj S <75058821+Nagarajjj@users.noreply.github.com>
2022-10-03 10:16:44 +0530

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,77 @@
+﻿using CustomerManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            Nagaraj_DBContext _context = new Nagaraj_DBContext();
+            var model = _context.CustomerTable.ToList();
+            return View(model);
+
+        }
+        public IActionResult Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new Exception("Id must be set");
+            int aId = int.Parse(id);
+
+            Nagaraj_DBContext context = new Nagaraj_DBContext();
+            var model = context.CustomerTable.FirstOrDefault((a) => a.CustId == aId);
+            if (model == null) throw new Exception("Customer not found");
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Edit(CustomerTable model)
+        {
+            Nagaraj_DBContext _context = new Nagaraj_DBContext();
+            var selected = _context.CustomerTable.FirstOrDefault((a) => a.CustId == model.CustId);
+            if (selected == null) throw new Exception("Failed to get the Customer details");
+            selected.CustPhoto = model.CustPhoto;
+            selected.CustName = model.CustName;
+            selected.CustAddress = model.CustAddress;
+            selected.CustBarrowedBooks = model.CustBarrowedBooks;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+
+
+        }
+
+        public IActionResult AddNew()
+        {
+            return View(new CustomerTable());
+        }
+        [HttpPost]
+        public IActionResult AddNew(CustomerTable model)
+        {
+            Nagaraj_DBContext _context = new Nagaraj_DBContext();
+            _context.CustomerTable.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+                throw new Exception("ID Must be set");
+            int CId = int.Parse(id);
+            Nagaraj_DBContext _context = new Nagaraj_DBContext();
+            var model = _context.CustomerTable.FirstOrDefault((e) => e.CustId == CId);
+      
+            if (model == null) throw new Exception("Customer not found");
+            
+            _context.CustomerTable.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+    }
+
+    
+}
---
+++ Migrations/20220930101815_new.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CustomerManagement.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerTable",
+                columns: table => new
+                {
+                    CustId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustName = table.Column<string>(nullable: true),
+                    CustAddress = table.Column<string>(nullable: true),
+                    CustBarrowedBooks = table.Column<int>(nullable: false),
+                    CustPhoto = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerTable", x => x.CustId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CustomerTable",
+                columns: new[] { "CustId", "CustAddress", "CustBarrowedBooks", "CustName", "CustPhoto" },
+                values: new object[] { 1, "Benagaluru", 2, "Nagaraj", "https://i.kinja-img.com/gawker-media/image/upload/t_original/ijsi5fzb1nbkbhxa2gc1.png" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerTable");
+        }
+    }
+}
